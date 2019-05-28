687d33b698d6c75a581c870bfc070df6b237c70b
Ricardo <pareja.r@gmail.com>
2019-05-28 00:21:46 -0300

    Updates

---
+++ precache-manifest.a70afad4836dcaaa4406d86e178e38cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faca2abf095a880a21df3b7ab65500bf",
+    "url": "/github_users/index.html"
+  },
+  {
+    "revision": "c0b2510fbb22e17bfa45",
+    "url": "/github_users/static/css/main.467d5276.chunk.css"
+  },
+  {
+    "revision": "1ce0fb9bd0e7a5174bc6",
+    "url": "/github_users/static/js/2.c7116e76.chunk.js"
+  },
+  {
+    "revision": "c0b2510fbb22e17bfa45",
+    "url": "/github_users/static/js/main.6b5b8f09.chunk.js"
+  },
+  {
+    "revision": "98cb0097e28f8e5572f4",
+    "url": "/github_users/static/js/runtime~main.b7024680.js"
+  }
+]);
